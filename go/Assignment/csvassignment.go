fbb67b0025d9b9c2d7f01dae9e5acdebeee62fb3
Manish Raut <manish@infracloud.io>
2019-09-04 17:30:00 +0530

    modified variable names and added index for top 5 users

--- go/Assignment/csvassignment.go
+++ go/Assignment/csvassignment.go
@@ -12,7 +12,14 @@ import (
 )
 
 type build struct {
-	UID, UserID, BRtime, BEtime, BFtime, BIndicat, Bstatus, Bimgsize string
+	UniqueBuildID,
+	UserID,
+	BuildReqReceived,
+	BuildExecutionStart,
+	BuildExecutionEnd,
+	BuildDeleteIndicator,
+	BuildExitCode,
+	BuiltImageSize string
 }
 
 func inTimeSpan(start, end, check time.Time) bool {
@@ -48,11 +55,11 @@ func main() {
 
 	start, _ := time.Parse(time.RFC3339, "2018-10-30T02:47:31-04:00")
 	end, _ := time.Parse(time.RFC3339, "2018-11-02T04:06:03-04:00")
-	count := 0
-	count2 := 0
-	var tcount []string
+	BuildCount := 0
+	ExitCodeCount := 0
+	IndexNum := 1
+	var TotalCount []string
 	var UsrID []string
-
 	//Read from CSV file
 	filePath := filepath.Join(getHome(), "Downloads", "data.csv")
 	csvfile, err := os.Open(filePath)
@@ -77,30 +84,30 @@ func main() {
 		}
 
 		var data = build{
-			UID:      record[0],
-			UserID:   record[1],
-			BRtime:   record[2],
-			BEtime:   record[3],
-			BFtime:   record[4],
-			BIndicat: record[5],
-			Bstatus:  record[6],
-			Bimgsize: record[7],
+			UniqueBuildID:        record[0],
+			UserID:               record[1],
+			BuildReqReceived:     record[2],
+			BuildExecutionStart:  record[3],
+			BuildExecutionEnd:    record[4],
+			BuildDeleteIndicator: record[5],
+			BuildExitCode:        record[6],
+			BuiltImageSize:       record[7],
 		}
-
-		if data.Bstatus == "0" {
-			count2++
+		// Check for the build exit codes, if 0 increment the counter
+		if data.BuildExitCode == "0" {
+			ExitCodeCount++
 		}
-		tcount = append(tcount, data.Bstatus)
+		TotalCount = append(TotalCount, data.BuildExitCode)
 
-		in, _ := time.Parse(time.RFC3339, data.BFtime)
+		in, _ := time.Parse(time.RFC3339, data.BuildExecutionEnd)
 		if inTimeSpan(start, end, in) {
-			count++
+			BuildCount++
 			//	fmt.Println(in, "is between", start, "and", end, ".")
 		}
 		UsrID = append(UsrID, data.UserID)
 	}
 
-	fmt.Printf("Nubmer of builds beetween the range date from %v to %v are %v\n", start, end, count)
+	fmt.Printf("Nubmer of builds beetween the range date from %v to %v are: %d\n", start, end, BuildCount)
 
 	RepeatMap := repeatCount(UsrID)
 
@@ -122,14 +129,16 @@ func main() {
 	sort.Slice(ss, func(i, j int) bool {
 		return ss[i].Value > ss[j].Value
 	})
-
+	//Print the top 5 values
+	fmt.Println("Top 5 users are-")
 	for _, kv := range ss[:5] {
-		fmt.Printf("UserId : %s, Count : %d\n", kv.Key, kv.Value)
+		fmt.Printf("%d. UserId : %s, Count : %d\n", IndexNum, kv.Key, kv.Value)
+		IndexNum++
 
 	}
 
-	//fmt.Println("Number of successful builds are ", count2)
-	//fmt.Println("Total count is:", len(tcount))
-	sRate := (float64(count2) / float64(len(tcount))) * 100
+	//fmt.Println("Number of successful builds are ", ExitCodeCount)
+	//fmt.Println("Total BuildCount is:", len(TotalCount))
+	sRate := (float64(ExitCodeCount) / float64(len(TotalCount))) * 100
 	fmt.Printf("Success Rate is: %v%%\n", sRate)
 }
